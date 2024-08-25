64fb28625f9122cc4df43e1a2749f08c2e771cbd
TPL822_6 <tpl822_6@TPL822-6s-MBP.attlocal.net>
2024-08-24 19:37:24 -0700

    done with kc6

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import fetch from 'node-fetch';
+const app = express();
+const port = 3000;
+
+
+// Replace 'YOUR_OAUTH_TOKEN' with your actual OAuth token
+const oauthToken = 'PCPJI42WI4DAMJBTKC2T';
+
+// information about your user account
+fetch(`https://www.eventbriteapi.com/v3/users/me/?token=${oauthToken}`)
+.then((response) => response.json())
+.then((users) => {console.log(users)})
+
+//retrieve by catogory
+fetch(`https://www.eventbriteapi.com/v3/categories/?token=${oauthToken}`)
+.then((response) => response.json())
+.then((events) => {console.log(events)})
+
+// retrieve organization
+fetch(`https://www.eventbriteapi.com/v3/users/me/organizations/?token=${oauthToken}`)
+.then((response) => response.json())
+.then((events) => {console.log(events)})
+
+
+//Retrieve Attendee Report
+fetch(`https://www.eventbriteapi.com/v3/reports/attendees/?token=${oauthToken}`)
+.then((response) => response.json())
+.then((events) => {console.log(events)})
+
+// retrieve venues
+fetch(`https://www.eventbriteapi.com/v3/venues/?token=${oauthToken}`)
+.then((response) => response.json())
+.then((events) => {console.log(events)})
+
+
+
+app.listen(port, () => {
+ console.log(`Server running at http://localhost:${port}`);
+});
+
